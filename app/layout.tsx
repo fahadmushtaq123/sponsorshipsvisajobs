a5d6ca766c3879b1a14e49a68331166657078090
fahadmushtaq123 <fahadmushtaq@sanpak.biz>
2025-08-26 00:25:09 +0500

    AddMonetagScriptTag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <script src="https://fpyf8.com/88/tag.min.js" data-zone="166261" async data-cfasync="false"></script>
         <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
       </body>
     </html>
