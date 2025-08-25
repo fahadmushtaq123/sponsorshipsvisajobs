9f92a07fbe0a7de1112bd6b05636198da5594ea7
fahadmushtaq123 <fahadmushtaq@sanpak.biz>
2025-08-26 00:32:58 +0500

    RemoveMonetag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,6 @@ import ClientLayoutWrapper from './ClientLayoutWrapper';
 export const metadata = {
   title: 'Sponsorship Visa Jobs - Find Your Next Career Opportunity',
   description: 'Discover thousands of job opportunities across various industries and locations. Your ultimate destination for job searching and career growth.',
-  other: {
-    monetag: '22f8f07698e7316e5ac699e84e0b77c2',
-  },
 };
 
 export default function RootLayout({
@@ -19,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <script src="https://fpyf8.com/88/tag.min.js" data-zone="166261" async data-cfasync="false"></script>
         <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
       </body>
     </html>
