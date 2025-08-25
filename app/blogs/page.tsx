0b6bbc92fa8d72a46092819098f681363b90109a
fahadmushtaq123 <fahadmushtaq@sanpak.biz>
2025-08-26 01:17:31 +0500

    "UpdateBlogScript"

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -89,7 +89,7 @@ export default function Blogs() {
 
   return (
     <Container className="mt-5">
-      <script src="//upskittyan.com/ntfc.php?p=9773631" data-cfasync="false" async onerror="_aijgub()" onload="_nupdjki()"></script>
+      <script src="https://shoukigaigoors.net/act/files/tag.min.js?z=9773631" data-cfasync="false" async></script>
       <h1 className="text-center mb-4">Blogs</h1>
       <Row>
         <Col md={isAdmin ? 8 : 12}>
