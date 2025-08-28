42ff470aff074627f0ccd80340696e2f75ed8534
fahadmushtaq123 <78116682+fahadmushtaq123@users.noreply.github.com>
2025-08-29 02:00:48 +0500

    fix: Add generateStaticParams to fix build error

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,5 +1,14 @@
 import JobDetailsClient from './JobDetailsClient';
 
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:3000/api/jobs');
+  const jobs = await res.json();
+ 
+  return jobs.map((job: { _id: string }) => ({
+    id: job._id,
+  }));
+}
+
 export default function JobDetailsPage() {
   return <JobDetailsClient />;
 }
