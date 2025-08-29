b563d2ebf0a99ddca367626af3072ea5b0e198b5
fahadmushtaq123 <78116682+fahadmushtaq123@users.noreply.github.com>
2025-08-30 04:29:26 +0500

    fix: Remove generateStaticParams from job details page

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -1,34 +1,4 @@
 import JobDetailsClient from './JobDetailsClient';
-import { MongoClient, ServerApiVersion } from 'mongodb';
-
-const uri = "mongodb+srv://sponsorshipsvisajobs-db:Sanpak1122@cluster0.28a3dng.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-export async function generateStaticParams() {
-  if (!uri) {
-    return [];
-  }
-
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
-  try {
-    await client.connect();
-    const database = client.db("job_board_db");
-    const jobsCollection = database.collection("jobs");
-    const jobs = await jobsCollection.find({}).toArray();
-
-    return jobs.map((job) => ({
-      id: job._id.toString(),
-    }));
-  } finally {
-    await client.close();
-  }
-}
 
 export default function JobDetailsPage() {
   return <JobDetailsClient />;
